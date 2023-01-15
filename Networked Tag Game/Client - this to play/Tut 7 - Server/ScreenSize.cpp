4d6c625dd5483089fb0fb1839ff28723eab89474
Adam Mcguigan <c00248515@itcarlow.ie>
2023-01-15 19:25:10 +0000

    Adding the networked game of tag so far
    I have changed the method that im creating the game now, i had tried to implement the advanced tutorial code but found it easier to, keep the client and server separate for now. Need to add more visuals to the game

---
+++ Networked Tag Game/Client - this to play/Tut 7 - Server/ScreenSize.cpp
@@ -0,0 +1,4 @@
+#include"ScreenSize.h"
+
+unsigned int ScreenSize::width = 1000; // 2400 
+unsigned int ScreenSize::height = 1000; // 1600
---
+++ Networked Tag Game/Client - this to play/Tut 7 - Server/ScreenSize.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct ScreenSize
+{
+public:
+
+	static unsigned int width;
+	static unsigned int height;
+};
+
